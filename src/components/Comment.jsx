b51ebdf7429a89409e7e10c2c53be686b589ffe3
Daniel Alves <11daniel.neves.alves@gmail.com>
2025-08-21 22:39:26 -0300

    CORRIGIR NOME DA CLASSE PARA authorAndTime

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -13,7 +13,7 @@ export function Comment() {
 			<div className={styles.commentBox}>
 				<div className={styles.commentContent}>
 					<header>
-						<div className={styles.authorAndTimes}>
+						<div className={styles.authorAndTime}>
 							<strong>Daniel Alves</strong>
 							<time title="11 de maio às 08:30h" dateTime="2025-05-11 08:13:30">
 								Cerca de 2 horas atrás
