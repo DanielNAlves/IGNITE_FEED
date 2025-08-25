277c907c439dfbfa3ff7de219e537e322ed36d21
Daniel Alves <11daniel.neves.alves@gmail.com>
2025-08-25 19:28:40 -0300

    ADICIONA LISTA DE POSTS COM AUTOR, CONTEÚDO E DATA

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,43 @@ import { Sidebar } from "./components/sidebar";
 import styles from "./App.module.css";
 import "./global.css";
 
+const posts = [
+	{
+		id: 1,
+		author: {
+			avatarUrl: "https://github.com/DanielAlves.png",
+			nome: "Daniel Alves",
+			role: "Dev front-end"
+		},
+
+		content: [
+		{type: 'paragraph', content: 'Fala galeraa 👋'},
+		{type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀{" "}'}, 
+		{type: 'link', content: '👉 <a href="#">jane.design/doctorcare</a>'},		
+		],
+
+		publishedAt: new Date('2019-11-23 16:00:00'),
+
+	},
+	{
+		id: 2,
+		author: {
+			avatarUrl: "https://github.com/DanielAlves.png",
+			nome: "Daniel V N Alves",
+			role: "Dev front-end"
+		},
+
+		content: [
+		{type: 'paragraph', content: 'Fala galeraa 👋'},
+		{type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀{" "}'}, 
+		{type: 'link', content: '👉 <a href="#">jane.design/doctorcare</a>'},		
+		],
+
+		publishedAt: new Date('2019-11-20 15:30:00'),
+
+	},
+];
+
 export function App() {
 	return (
 		<div>
@@ -12,8 +49,14 @@ export function App() {
 			<div className={styles.wrapper}>
 				<Sidebar />
 				<main>
-					<Post author="Daniel Alves" content="Lorem ipsum dolor sit amet." />
-					<Post author="Teste" content="Um post muito legal." />
+					{posts.map(post => {
+					return	(
+						<Post 
+						  author={post.author}
+						  content={post.content}
+						  publishedAt={post.publishedAt}
+					  />)
+					})}
 				</main>
 			</div>
 		</div>
