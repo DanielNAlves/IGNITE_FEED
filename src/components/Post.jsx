f3ab946e218042244eda7e424c5fc94d61e93bb6
Daniel Alves <11daniel.neves.alves@gmail.com>
2025-08-28 23:58:30 -0300

    FEAT: ADICIONAR props AO COMPONENTE Post PARA EXIBIR DADOS DINÂMICOS

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,32 +1,43 @@
+import {format, formatDistanceToNow} from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { Avatar } from './Avatar';
 import { Comment } from "./Comment";
 
 import styles from "./Post.module.css";
 
-export function Post(props) {
-	console.log(props);
+export function Post({author, publishedAt, content}) {
+	const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", { locale: ptBR })
+
+	const publishedDateRelative = formatDistanceToNow(publishedAt, { locale: ptBR, addSuffix: true })
+
 	return (
 		<article className={styles.post}>
 			<header>
 				<div className={styles.author}>
 					<Avatar
-						src="https://github.com/danielnalves.png"
-						alt="Daniel Alves"
+						src={author.avatarUrl}
 					/>
 
 					<div className={styles.authorInfo}>
-						<strong>Daniel Alves</strong>
-						<span>Web Developer</span>
+						<strong>{author.name}</strong>
+						<span>{author.role}</span>
 					</div>
 				</div>
 
-				<time title="11 de maio às 08:30h" dateTime="2025-05-11 08:13:30">
-					Publicado há 1 hora
+				<time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+					{publishedDateRelative}
 				</time>
 			</header>
 
 			<div className={styles.content}>
-				
+				{content.map(line => {
+					if(line.type === 'paragraph') {
+						return <p>{line.content}</p>
+					} else if (line.type === 'link') {
+						return <p><a href="#">{line.content}</a></p>
+					}
+				})}
 			</div>
 
 			<form className={styles.commentForms}>
