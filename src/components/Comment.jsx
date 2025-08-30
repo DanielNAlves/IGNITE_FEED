31ed64279a659b73d44741447ce693ae4a707696
Daniel Alves <11daniel.neves.alves@gmail.com>
2025-08-29 23:02:49 -0300

    FEAT: ADICIONAR PROP CONTENT AO COMPONENTE COMMET

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,7 @@ import styles from "./Comment.module.css";
 import { Trash, ThumbsUp } from "phosphor-react";
 import { Avatar } from './Avatar';
 
-export function Comment() {
+export function Comment({content}) {
 	return (
 		<div className={styles.comment}>
 			<Avatar hasBorder={false}
@@ -25,7 +25,7 @@ export function Comment() {
 						</button>
 					</header>
 
-					<p>Muito bom Devon, parabéns!! 👏👏</p>
+					<p>{content}</p>
 				</div>
 
 				<footer>
