3080ae9929df3c86216af5b405f1dceb09e581d7
Daniel Alves <11daniel.neves.alves@gmail.com>
2025-09-06 00:23:28 -0300

    feat(comment): adiciona funcionalidade de like nos comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,14 +1,22 @@
-import styles from "./Comment.module.css";
 import { Trash, ThumbsUp } from "phosphor-react";
+import	{ useState } from "react";
 import { Avatar } from './Avatar';
+import styles from "./Comment.module.css";
 
 export function Comment({ content, onDeleteComment }) {
+
+	const[likeCount, setLikeCount] = useState(0);
 
 	function handleDeleteComment() {
 		console.log('Deletar comentário');
 
 		onDeleteComment(content);
 	}
+
+	function handleLikeComment() {
+		setLikeCount (likeCount + 1)
+	}
+
 
 	return (
 		<div className={styles.comment}>
@@ -36,9 +44,9 @@ export function Comment({ content, onDeleteComment }) {
 				</div>
 
 				<footer>
-					<button>
+					<button onClick={handleLikeComment}>
 						<ThumbsUp size={20} />
-						Aplaudir <span>20</span>
+						Aplaudir <span>{likeCount}</span>
 					</button>
 				</footer>
 			</div>
